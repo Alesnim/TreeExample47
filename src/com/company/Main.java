2952dbd3a85d8b542a65177a35301993814fd77f
Aleksey <alesnim@gmail.com>
2021-02-22 18:42:10 +0300

    add BinaryTree

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,27 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        MyTree tree = new MyTree(new Node("one"));
+    /*    MyTree tree = new MyTree(new Node("one"));
         tree.printTree();
-		System.out.println("++++++++");
+		System.out.println("+++++++++++++++++++++++++");
         tree.addChild(new Node("two"));
         tree.addChild("two", new Node("three"));
         tree.addChild("one", new Node("four"));
         tree.addChild("four", new Node("five"));
         tree.addChild("two", new Node("six"));
-        tree.printTree();
+        tree.printTree();*/
+
+        /*TreeSet set = new TreeSet<String>();
+        set.add("one");
+        set.add("two");
+        System.out.println(set);
+
+        TreeMap treeMap = new TreeMap<Integer, String>();
+        treeMap.put(1, "Один");*/
+
+
+        BinaryTree treeBinary = BinaryTree.createRandomIntegerTree(10);
+        BinaryTree.drawTree(treeBinary);
+
     }
 }
